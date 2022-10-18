829f69f822f0cafb59025f274462b531f6840a2f
Daniel Gómez <88523667+Danigom11@users.noreply.github.com>
2022-10-18 19:35:14 +0200

    ejercicio1

--- src/com/company/ejercicio1/Ejercicio1.java
+++ src/com/company/ejercicio1/Ejercicio1.java
@@ -1,7 +1,46 @@
-package ejercicio1;
+package com.company.ejercicio1;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
+
+        //TIPOS PRIMITIVOS
+
+        byte num1 = 1;
+        short num2 = 2;
+        int num3 = 3;
+        long num4 = 4;
+
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        char caracter1 = 'a';
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // TIPO CLASE
+
+        String nombre = "Dani";
+        String apellido = "Sastre";
+
+        // TIPO ENVOLTORIO
+
+        Integer numero = null;
+        Long numero2 = 2L;
+
+        System.out.println("Byte: " + num1);
+        System.out.println("Short: " + num2);
+        System.out.println("Int: " + num3);
+        System.out.println("Long: " + num4);
+        System.out.println("Float: " + decimal1);
+        System.out.println("Double: " + decimal2);
+        System.out.println("Char: " + caracter1);
+        System.out.println("Boolean: " + verdadero);
+        System.out.println("Boolean: " + falso);
+        System.out.println("String: " + nombre);
+        System.out.println("String: " + apellido);
+        System.out.println("Integer: " + numero);
+        System.out.println("Long: " + numero2);
 
     }
 }
