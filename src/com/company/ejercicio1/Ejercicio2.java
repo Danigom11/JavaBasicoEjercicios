2c618784b1ac4a5a9df1d7cd1231a8993bc431ab
Daniel Gómez <88523667+Danigom11@users.noreply.github.com>
2022-10-19 19:45:07 +0200

    ejercicio1

---
+++ src/com/company/ejercicio1/Ejercicio2.java
@@ -0,0 +1,17 @@
+package com.company.ejercicio1;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        double precio = 100.00;
+        System.out.println("Precio sin IVA: " + precio);
+        double resultado = getPriceIva(precio);
+        System.out.println("Precio con IVA: " + resultado + "€");
+    }
+
+    static double getPriceIva(double precio) {
+        return precio * 1.21;
+    }
+
+
+}
