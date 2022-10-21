b538463ed68ed2469d848aff5ea1af7533df9e3e
Daniel Gómez <88523667+Danigom11@users.noreply.github.com>
2022-10-21 17:53:46 +0200

    ejercicio3

---
+++ src/ejercicios/Ejercicio3.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        String[] textos = {"En un ", "lugar de ", "la Mancha, ", "de cuyo nombre ", "no quiero acordarme..."};
+
+        for (int i = 0; i < textos.length; i++) {
+            System.out.print(textos[i]);
+        }
+    }
+}
