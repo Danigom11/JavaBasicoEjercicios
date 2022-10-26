8134af285b27e0e8cc814cf64e0c60d6e244259f
Daniel Gómez <88523667+Danigom11@users.noreply.github.com>
2022-10-26 10:36:50 +0200

    ejercicio4

--- src/ejercicios/ejercicio4/SmartDevice.java
+++ src/ejercicios/ejercicio4/SmartDevice.java
@@ -22,4 +22,5 @@ public class SmartDevice {
                 ", pixelsCamara=" + pixelsCamara +
                 '}';
     }
+
 }
---
+++ src/ejercicios/ejercicio5/Main.java
@@ -0,0 +1,13 @@
+package ejercicios.ejercicio5;
+
+public class Main {
+
+    static CocheCRUD cocheCrud = new CocheCRUDImpl();
+    public static void main(String[] args) {
+
+        cocheCrud.save();
+        cocheCrud.findAll();
+        cocheCrud.delete();
+
+    }
+}
